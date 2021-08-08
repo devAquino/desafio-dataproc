5b989ebd1781339b2e09ba39fb95b7710a9a9ebc
devAquino <devaquinoreginaldo@gmail.com>
2021-08-08 23:07:47 +0000

    add new files

--- contador.py
+++ contador.py
@@ -1,7 +1,7 @@
 import sys
 from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
-    sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    words = sc.textFile("gs://bucket-desafio-dataproc/livro.txt").flatMap(lambda line: line.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    sc.parallelize(wordCounts.collect()[0:10]).saveAsTextFile("gs://bucket-desafio-dataproc/my-resultado")
+    sc = SparkContext("local","Meu-Desafio-Dataproc")
+    words = sc.textFile("gs://bucket-meu-desafio-dataproc/livro.txt").flatMap(lambda line: line.split(" ")).filter(lambda x: len(x) > 0)
+    wordcounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
+    sc.parallelize(wordcounts.collect()[:10]).saveAsTextFile("gs://bucket-meu-desafio-dataproc/meu-resultado")
